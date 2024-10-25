f92023229fd31ad270c8e6820b83f83b8c3885d7
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2024-10-25 21:29:50 +0700

    Update github action configuration.

--- src/main/kotlin/io/narok/jscgs/plugins/Routing.kt
+++ src/main/kotlin/io/narok/jscgs/plugins/Routing.kt
@@ -1,7 +1,6 @@
 package io.narok.jscgs.plugins
 
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
-import io.ktor.http.ContentType
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
